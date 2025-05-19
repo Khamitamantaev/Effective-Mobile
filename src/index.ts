546b7bb85a6d2039543081298696750f40d3c4a7
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2025-05-20 02:54:49 +0500

    create appeal working correctly and added json parse

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
-import express from 'express';
-import { PrismaClient } from '@prisma/client';
-import { Request, Response } from 'express';
+import express from "express";
+import { PrismaClient } from "../generated/prisma";
+import { Request, Response } from "express";
 
 const app = express();
+const prisma = new PrismaClient();
+const port = process.env.PORT;
+app.use(express.json())
 
-const port = process.env.PORT
+app.post("/appeal", async (req: Request, res: Response) => {
+  const { topic, text } = req.body;
+  if (!topic || !text) {
+    return res.status(400).json({ error: "Topic and text are required" });
+  }
 
-app.get('/', (req, res) => {
-  res.send('Hello from Khammerson!')
-})
+  try {
+    const appeal = await prisma.appeal.create({
+      data: {
+        topic,
+        text,
+      },
+    });
+    res.status(201).json(appeal);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to create appeal" });
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Efeective app listening on port ${port}`)
-})
+  console.log(`Efeective app listening on port ${port}`);
+});
